443a233372148bc1d62a764ed527cdfaa4bd6471
AMaskedDev <153927002+AMaskedDev@users.noreply.github.com>
2024-07-10 13:22:29 +0300

    Update my-version.lua

--- my-version.lua
+++ my-version.lua
@@ -430,7 +430,7 @@ function Sections:AddTextBox(Name, CallBack)
 		Size = UDim2.new(0.950, 0, 0, 30),
 		ZIndex = 2,
 		Image = "rbxassetid://5028857472",
-		ImageColor3 = Color3.fromRGB(15, 15, 15)
+		ImageColor3 = Color3.fromRGB(15, 15, 15),
 		ScaleType = Enum.ScaleType.Slice,
 		SliceCenter = Rect.new(2, 2, 298, 298)
 	})
